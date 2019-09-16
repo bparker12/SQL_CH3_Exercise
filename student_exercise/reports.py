a7276a48a1a401e1d641116168b7f216d78b486a
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-09-16 10:26:17 -0500

    getting things to work

---
+++ student_exercise/reports.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+class Student():
+
+    def __init__(self, first, last, handle, cohort):
+        self.first_name = first
+        self.last_name = last
+        self.slack_handle = handle
+        self.cohort = cohort
+
+class StudentExerciseReport:
+
+        # """Methods for reports on the Student Exercises database"""
+
+    def __init__(self):
+        self.db_path = "/Users/stuff/workspace/python/SQL/Ch3_StudentExercise3/student_exercise/studentexercises.db"
+
+    def create_student(self, cursor, row):
+        return Student(row[1], row[2], row[3], row[5])
+
+    def all_students(self):
+
+        with sqlite3.connect(self.db_path) as conn:
+            conn.row_factory = self.create_student
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select s.Id,
+                s.first_name,
+                s.last_name,
+                s.slack_handle,
+                s.CohortId,
+                c.Name
+            from Student s
+            join Cohort c on s.CohortId = c.Id
+            order by s.CohortId
+            """)
+
+            all_students = db_cursor.fetchall()
+
+            for student in all_students:
+                print(f'{student.first_name} {student.last_name} is in {student.cohort}')
+
+
+reports = StudentExerciseReport()
+reports.all_students()
+
